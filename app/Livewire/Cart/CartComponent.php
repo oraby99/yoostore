fd99d74b128c75c103f6fe1d7c968ad2e5cff45f
abdelrahmannshoieeb <abdoshoieb93@gmail.com>
2024-10-25 22:59:21 +0300

    cart page

--- app/Livewire/Cart/CartComponent.php
+++ app/Livewire/Cart/CartComponent.php
@@ -2,12 +2,91 @@
 
 namespace App\Livewire\Cart;
 
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CartComponent extends Component
 {
+    public $products; 
+    public $total = 0; 
+    public $discount = 0; 
+    public $quantities = [];
+
+    public function mount()
+    {
+        $this->products = Cart::where('user_id', Auth::user()->id)->get();
+
+        foreach ($this->products as $product) {
+            $this->quantities[$product->id] = $product->quantity; 
+        }
+    }
+
+    public function incrementQuantity($productId)
+    {
+        $this->quantities[$productId]++;
+        $item = Cart::find($productId);
+        $item->update(['quantity' => $this->quantities[$productId]]);
+    }
+
+    public function decrementQuantity($productId)
+    {
+        if ($this->quantities[$productId] > 1) {
+            $this->quantities[$productId]--;
+            $item = Cart::find($productId);
+            $item->update(['quantity' => $this->quantities[$productId]]);
+        } else {
+            $this->removeProduct($productId);
+        }
+    }
+
+    public function removeProduct($productId)
+    {
+        $this->products = $this->products->filter(function ($product) use ($productId) {
+            return $product->id != $productId;
+        });
+
+        Cart::where('id', $productId)->delete();
+        unset($this->quantities[$productId]);
+    }
+
+    private function calculateTotal()
+    {
+        $this->total = 0;
+        foreach ($this->products as $product) {
+            $this->total += $product->productDetail->typeprice * $this->quantities[$product->id];
+        }
+        return $this->total;
+    }
+    public function calculateTotalWithDiscount()
+    {
+        $this->discount = 0;
+    
+        foreach ($this->products as $item) {
+            $product = Product::find($item->product_id);
+            if ($product) {
+                $this->discount += $product->discount;
+            }
+        }
+    }
+
+
+    public function checkout()
+
+    {
+        
+        return redirect()->route('checkout');
+    }
+    
     public function render()
     {
-        return view('livewire.cart.cart-component');
+        // Refresh the products to keep them in sync
+        $this->products = Cart::where('user_id', Auth::user()->id)->get();
+
+        // dd($this->products);
+        return view('livewire.cart.cart-component', [
+            'total' => $this->calculateTotal()
+        ]);
     }
 }
--- resources/views/livewire/cart/cart-component.blade.php
+++ resources/views/livewire/cart/cart-component.blade.php
@@ -1,3 +1,61 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="main">
+    <div class="shopping-cart">
+        <h2>Shopping Card</h2>
+        <table>
+            <thead>
+                <tr style="background-color: #E4E7E9;">
+                    <th>Products</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>SubTotal</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product) 
+                <tr>
+                <td>
+                    <i class="fa-regular fa-circle-xmark text-danger" wire:click="removeProduct({{ $product->id }})" style="cursor:pointer;"></i> 
+                    <img src="{{ asset('yoostore/images/xaomi.png') }}" alt="{{ $product->name }}">
+                    <span>{{ $product->name }}</span>
+                </td>
+                <td>{{ $product->productDetail->typeprice }} KWD</td>
+                <td class="quantity">
+                    <button wire:click="decrementQuantity({{ $product->id }})">-</button>
+                    <span>{{ $quantities[$product->id] }}</span>
+                    <button wire:click="incrementQuantity({{ $product->id }})">+</button>
+                </td>
+                <td>{{ $product->productDetail->typeprice * $quantities[$product->id] }} KWD</td>
+            </tr>
+                @endforeach
+              
+            </tbody>
+        </table>
+        <div class="cart-buttons">
+            <button class="return-button"><i class="fa-solid fa-arrow-left mx-3"></i>Return to Shop</button>
+            <button class="update-button">Update Cart <i class="fa-solid fa-arrow-right mx-3"></i></button>
+        </div>
+    </div>
+    <div class="cart-summary">
+        <div class="cart-totals">
+            <h3>Card Totals</h3>
+            <div class="totals-row">
+                <span>Sub-total</span>
+                <span>{{ $total }} KWD</span>
+            </div>
+            <div class="totals-row">
+                <span>Shipping</span>
+                <span>Free</span>
+            </div>
+            <div class="totals-row d-none">
+                <span>Discount</span>
+                <span>{{ $discount }} KWD</span>
+            </div>
+            <hr>
+            <div class="totals-row total">
+                <span>Total</span>
+                <span>{{ $total}} KWD</span>
+            </div>
+            <button class="checkout-button" wire:click="checkout()">Proceed to Checkout</button>
+        </div>
+    </div>
 </div>
