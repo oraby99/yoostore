f301e4f14bf65d2b6cabe03bdcea925f2b653983
Ahmed Oraby <ahmed.oraby@ocoda.com>
2024-12-21 16:16:03 +0200

    updates

--- resources/views/vendor/filament/components/chats.blade.php
+++ resources/views/vendor/filament/components/chats.blade.php
@@ -5,6 +5,7 @@
         @endphp
         @foreach ($chatsGroupedByUser as $userName => $chatsByUser)
             <div class="border rounded-lg p-4 bg-gray-100 shadow-md">
+                <!-- User Section -->
                 <h2 
                     class="font-bold text-lg cursor-pointer" 
                     onclick="toggleSection('user-{{ str_replace(' ', '-', $userName) }}')">
@@ -14,20 +15,24 @@
                 <div 
                     class="hidden mt-4 space-y-4" 
                     id="user-{{ str_replace(' ', '-', $userName) }}">
+
                     @php
                         $chatsGroupedByProduct = $chatsByUser->groupBy('product.name');
                     @endphp
+
                     @foreach ($chatsGroupedByProduct as $productName => $chatsByProduct)
+                        <!-- Product Section -->
                         <div class="border rounded-lg p-3 bg-gray-200">
                             <h3 
                                 class="font-semibold cursor-pointer" 
-                                onclick="toggleSection('product-{{ str_replace(' ', '-', $productName) }}')">
+                                onclick="toggleSection('product-{{ str_replace(' ', '-', $userName . '-' . $productName) }}')">
                                 Product Name: {{ $productName }}
                                 <span class="ml-2">+</span>
                             </h3>
                             <div 
                                 class="hidden mt-3 space-y-3" 
-                                id="product-{{ str_replace(' ', '-', $productName) }}">
+                                id="product-{{ str_replace(' ', '-', $userName . '-' . $productName) }}">
+
                                 @foreach ($chatsByProduct as $chat)
                                     <!-- Chat Section -->
                                     <div class="p-3 rounded bg-gray-300">
@@ -39,9 +44,11 @@
                                         </a>
                                     </div>
                                 @endforeach
+
                             </div>
                         </div>
                     @endforeach
+
                 </div>
             </div>
         @endforeach
